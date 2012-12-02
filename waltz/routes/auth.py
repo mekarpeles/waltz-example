4fc875c16781d216c837e1872495f11ed2102a37
Mek Karpeles <mek@ark.com>
2012-12-01 18:38:57 -0800

    Added session routes for login, register, etc

--- waltz/routes/auth.py
+++ waltz/routes/auth.py
@@ -7,13 +7,19 @@
 """
 
 import web
+from ballroom import security
 from ballroom.decorations import track
 from ballroom.dancers import session, render
 from configs.config import server
 
+print server['responses']
+# Events (message/responses)
+ALREADY_LOGGED = server['responses']['WARNING']['ALREADY_LOGGED']
+LOGGED_OUT = server['responses']['SUCCESS']['LOGGED_OUT']
+
 def redir2login(redir="/", msg=""):
     if web.ctx.homedomain:
-        return web.seeother("%s/login?redir=%s" % (web.ctx.homedomain, redir))
+        return web.seeother("{}/login?redir={}".format(web.ctx.homedomain, redir))
     return web.notfound()
 
 class Login:
@@ -23,8 +29,8 @@ class Login:
         #redirect2https(web.ctx, '/login')
         i = web.input(msg="", redir="", err="")
         if session().logged:
-            return "Already logged in"
-        return "TODO Login page"
+            return render().auth.login(resp=ALREADY_LOGGED)
+        return render().auth.login()
 
     def POST(self):
         i = web.input(username='', email='', password='', redir='/')
@@ -33,7 +39,7 @@ class Login:
         ## Implement your own logic here:
         #if (i.email and i.password) and auth_api._email_registered(i.email):
         #    if auth_api._passwords_match(i.email, i.password):
-        #        web.ctx.session['session'] = auth_api._update_session_login(SESSION(), i.email)
+        #        session() = auth_api._update_session_login(SESSION(), i.email)
         #        if i.redir:
         #            if not i.redir[0] == "/":
         #                i.redir = "/" + i.redir
@@ -45,9 +51,9 @@ class Login:
         #    return RENDER().login(msg=ERROR_LOGIN_PASSWD['key'])
         #return RENDER().login(msg=ERROR_ACT_CREDS['key'])
 
-        web.ctx.session().logged = True
-        web.ctx.session().username = i.username
-        raise web.seeother('/')
+        session().logged = True
+        session().username = i.username
+        raise web.seeother('/account')
 
 class Logout:
     @track
@@ -58,4 +64,16 @@ class Logout:
         session().kill()
         if i.redir:
             raise web.seeother(i.redir)
-        raise redir2login(redir='/login')
+        #raise redir2login(redir='/login')
+        return render().auth.login(resp=LOGGED_OUT)
+
+class Register:
+    @track
+    def GET(self):
+        i = web.input(redir='')
+        return render().auth.register()
+
+    def POST(self):
+        i = web.input(username='', email='', passwd1='', passwd2='',
+                      redir='')
+        return render().generic("registration workflow incomplete", "Registration backend coming soon, thanks for your patience!")
