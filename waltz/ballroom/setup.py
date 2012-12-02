f1fd59a356c37fbce8a0fc5b762c66614b13a538
Mek Karpeles <mek@ark.com>
2012-12-01 18:38:08 -0800

    Polishing the ballroom with template features and a security module

--- waltz/ballroom/setup.py
+++ waltz/ballroom/setup.py
@@ -12,28 +12,34 @@ from configs.config import server
 def dancefloor(web, urls, sessions=False, autoreload=False):
     app = web.application(urls, globals(), autoreload=autoreload)
 
-    _globals = {'ctx': web.ctx,}
+    _globals = {'ctx': web.ctx}
     slender  = web.template.render('templates/', globals=_globals)
     render  = web.template.render('templates/', base='base', globals=_globals)
-    _globals['render'] = render
+    _globals['render'] = slender
 
     if sessions:
         storage_method = web.session.DiskStore(server['paths']['sessions'])
-        session = start_sessions(web, app, storage_method)        
-        def session_hook(): web.ctx.session = session
-        app.add_processor(web.loadhook(session_hook))
+        session = init_sessions(web, app, storage_method)
+        def inject_session():
+            """closure; uncalled function which wraps session is
+            passed to the web loadhook and invoked elsewhere and at a
+            later point in time
+            """
+            web.ctx.session = session
+        app.add_processor(web.loadhook(inject_session))
+        _globals['session'] = session
 
     def render_hook(): web.ctx.render = render
     app.add_processor(web.loadhook(render_hook))
     return app
 
-
-def start_sessions(web, app, storage_method, **kwargs):
-    web.config.session_parameters['ignore_expiry'] = False
+def init_sessions(web, app, storage_method, **kwargs):
+    web.config.session_parameters['ignore_expiry'] = True
 
     def default_session(web_session):
         default_session = {'logged': False,
-                           'username': ''
+                           'username': '',
+                           'uid': -1 # user id
                            }
         web_session.update(default_session)
     session = web.session.Session(app, storage_method, initializer=default_session)
