6c3745ca82ddc77382fabc7aa81d6101942f06ca
Michael E. Karpeles <michael.karpeles@gmail.com>
2013-01-19 06:33:49 -0800

    Bumped dependency for lazydb to 0.1.6
    Bumped dependency for lazydb to 0.1.6 and incremented waltz to 0.1.4

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ from distutils.core import setup
 
 setup(
     name='waltz',
-    version='0.1.3',
+    version='0.1.4',
     url='http://github.com/mekarpeles/waltz',
     author='mek',
     author_email='michael.karpeles@gmail.com',
@@ -31,7 +31,7 @@ setup(
     scripts=[],
     license='LICENSE',
     install_requires=[
-        'lazydb >= 0.1.4',
+        'lazydb >= 0.1.6',
         'web.py >= 0.36',
         'lepl >= 5.1.3',
         'nose >= 1.1.2'
